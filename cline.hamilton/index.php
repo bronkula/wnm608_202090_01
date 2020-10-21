88f4e0a69a8c11140fbc2d3d23e6a4969f3efc01
Hamilton <hamdiggy@gmail.com>
2020-10-21 16:05:56 -0700

    module 7

---
+++ cline.hamilton/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>Store</title>
+
+   <?php include "parts/meta.php" ?>
+</head>
+<body>
+   
+   <?php include "parts/navbar.php" ?>
+
+
+
+   <div class="view-window" style="background-image:url(img/mountains001.jpg)">
+      <h2>Product Name</h2>
+   </div>
+   <div class="container">
+      <div class="card soft">
+         <h2>Welcome to the Store</h2>
+      </div>
+   </div>
+
+</body>
+</html>
