8be975d797b3e424dba11c4099e45a03b8022a62
Hamilton <hamdiggy@gmail.com>
2020-11-25 15:52:25 -0800

    about to change sort

---
+++ cline.hamilton/data/api.php
@@ -0,0 +1,102 @@
+<?php
+
+@include_once "../lib/php/functions.php";
+
+function getRequires($props) {
+   foreach($props as $prop) {
+      if(!isset($_GET[$prop])) return false;
+   }
+   return true;
+}
+
+
+function makeStatement($type) {
+
+   switch($type) {
+      case "products_all":
+         return MYSQLIQuery("SELECT *
+            FROM `products`
+            ORDER BY `date_create` DESC
+            LIMIT 12");
+         break;
+
+
+      case "product_by_id":
+         if(!getRequires(['id'])) return
+            ["error"=>"Missing Properties"];
+
+         return MYSQLIQuery("SELECT *
+            FROM `products`
+            WHERE `id` = ".$_GET['id']);
+         break;
+
+
+      case "products_by_category":
+         if(!getRequires(['category'])) return
+            ["error"=>"Missing Properties"];
+
+         return MYSQLIQuery("SELECT *
+            FROM `products`
+            WHERE `category` = '{$_GET['category']}'
+            LIMIT 12
+            ");
+         break;
+
+
+      case "price_above":
+         if(!getRequires(['price'])) return
+            ["error"=>"Missing Properties"];
+
+         return MYSQLIQuery("SELECT *
+            FROM `products`
+            WHERE `price` > '{$_GET['price']}'
+            ORDER BY price DESC
+            LIMIT 12
+            ");
+         break;
+
+      case "price_below":
+         if(!getRequires(['price'])) return
+            ["error"=>"Missing Properties"];
+
+         return MYSQLIQuery("SELECT *
+            FROM `products`
+            WHERE `price` < '{$_GET['price']}'
+            ORDER BY price DESC
+            LIMIT 12
+            ");
+         break;
+
+
+
+
+
+
+      case "search":
+         if(!getRequires(['s'])) return
+            ["error"=>"Missing Properties"];
+
+         return MYSQLIQuery("SELECT *
+            FROM `products`
+            WHERE `title` LIKE '%{$_GET['s']}%'
+            ORDER BY {$_GET['orderby']} {$_GET['orderby_direction']}
+            LIMIT {$_GET['limit']}
+            ");
+         break;
+
+
+
+
+      default: return ["error"=>"No Matched Type"];
+   }
+}
+
+
+// if(isset($_GET['t'])) {
+//    echo json_encode(
+//       makeStatement($_GET['t']),
+//       JSON_UNESCAPED_UNICODE |
+//       JSON_NUMERIC_CHECK
+//    );
+// }
+
--- cline.hamilton/product_list.php
+++ cline.hamilton/product_list.php
@@ -2,9 +2,57 @@
 
 include_once "lib/php/functions.php";
 include_once "parts/templates.php";
+include_once "data/api.php";
 
 
 
+setDefault('s','');
+setDefault('t','products_all');
+setDefault('orderby_direction','DESC');
+setDefault('orderby','date_create');
+setDefault('limit','12');
+
+
+
+
+function makeSortOptions() {
+   $options = [
+      ["date_create","DESC","Latest Products"],
+      ["date_create","ASC","Oldest Products"]
+   ];
+   foreach($options as [$orderby,$direction,$title]) {
+      echo "
+      <option data-orderby='$orderby' data-direction='$direction'
+      ".($_GET['orderby']==$orderby && $_GET['orderby_direction']==$direction ? "selected" : "").">
+      $title
+      </option>
+      ";
+   }
+}
+
+
+
+
+if(isset($_GET['t'])) {
+
+
+//    $filename = "data/api.php?".http_build_query($_GET);
+// echo $filename;
+   // $result = file_get_json($filename);
+
+
+
+   $result = makeStatement($_GET['t']);
+   $products = isset($result['error']) ? [] : $result;
+
+
+
+   // print_p($result);
+   // die;
+} else {
+   $result = makeStatement('products_all');
+   $products = isset($result['error']) ? [] : $result;
+}
 
 
 ?><!DOCTYPE html>
@@ -19,24 +67,45 @@ include_once "parts/templates.php";
    <?php include "parts/navbar.php" ?>
 
    <div class="container">
-         <h2>Product List</h2>
 
-         <div class="grid gap">
-           
-            <?php
+      <form action="product_list.php" method="get" class="hotdog stack">
+         <input type="search" name="s" placeholder="Search for a product"
+         value="<?= @$_GET['s'] ?>">
 
-            echo array_reduce(
-               MYSQLIQuery("
-                  SELECT *
-                  FROM products
-                  ORDER BY date_create DESC
-                  LIMIT 12
-               "),
-               'makeProductList'
-            );
+         <input type="hidden" name="orderby" value="<?=$_GET['orderby']?>">
+         <input type="hidden" name="orderby_direction" value="<?=$_GET['orderby_direction']?>">
+         <input type="hidden" name="limit" value="<?=$_GET['limit']?>">
 
-            ?>
+         <input type="hidden" name="t" value="search">
+         <button type="submit">Search</button>
+      </form>
+
+      <form action="product_list.php" method="get">
+         <input type="hidden" name="s" value="<?=$_GET['s']?>">
+         <input type="hidden" name="limit" value="<?=$_GET['limit']?>">
+         <input type="hidden" name="orderby">
+         <input type="hidden" name="orderby_direction">
+
+         <div class="form-select">
+            <select onchange="checkSort(this)">
+               <?=makeSortOptions()?>
+            </select>
          </div>
+      </form>
+
+      <h2>Product List</h2>
+
+      <div class="grid gap">
+        
+         <?php
+
+         echo array_reduce(
+            $products,
+            'makeProductList'
+         );
+
+         ?>
+      </div>
    </div>
 
 </body>
